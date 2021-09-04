7da56329959ba2aad30e73ffbc0215b9db5f99c2
irayya <ihiremath1991@gmail.com>
2021-09-04 18:34:53 +0530

    new color guess game

---
+++ memoryGame.js
@@ -0,0 +1,19 @@
+let  clrArr=['red','blue','green'];
+let box=document.getElementsByClassName('round')
+let getColor=[]
+function random(){
+for (let i=0;i<6;i++){
+     box[i].style.backgroundColor= clrArr[Math.floor(Math.random()*3)];
+    // console.log('hello')
+    //  console.log(box[i])
+ }
+}
+
+function clrHide() {
+    for (let i=0;i<6;i++){
+     setTimeout(function(){(box[i].style.backgroundColor='')},3000);
+  }
+}
+
+random()
+clrHide()

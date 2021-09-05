86ddf6ec84d88a38b13ff9ec0054401d54c712fe
irayya <ihiremath1991@gmail.com>
2021-09-05 23:33:35 +0530

    completed

--- memoryGame.js
+++ memoryGame.js
@@ -1,6 +1,7 @@
 let  clrArr=['red','blue','green'];
 let box=document.getElementsByClassName('round')
 let getColor=[]
+
 function random(){
 for (let i=0;i<6;i++){
      box[i].style.backgroundColor= clrArr[Math.floor(Math.random()*3)];
@@ -9,11 +10,11 @@ for (let i=0;i<6;i++){
  }
 }
 
-function clrHide() {
-    for (let i=0;i<6;i++){
-     setTimeout(function(){(box[i].style.backgroundColor='')},3000);
-  }
-}
+// function clrHide() {
+//     for (let i=0;i<6;i++){
+//      setTimeout(function(){(box[i].style.backgroundColor='')},3000);
+//   }
+// }
 
 random()
 clrHide()

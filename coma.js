cd9554275edc8e8c18426a50f776614e17644fd6
irayya <ihiremath1991@gmail.com>
2021-09-10 22:42:11 +0530

    new file uploaded

---
+++ coma.js
@@ -0,0 +1,8 @@
+function cal(x) {
+    x = x.toString();
+    var pattern = /(-?\d+)(\d{3})/;
+    while (pattern.test(x))
+        x = x.replace(pattern, "$1,$2");
+    return x;
+}
+document.getElementById("test").innerHTML = cal(1000000)
